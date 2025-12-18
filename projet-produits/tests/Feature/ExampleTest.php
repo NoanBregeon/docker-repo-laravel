47a2cac4e62b2ca7b576c6ed3f9aa33260c86c1f
Noan_Bregeon <furienocturne2.0@gmail.com>
2025-12-18 11:48:49 +0100

    Update ExampleTest.php

--- projet-produits/tests/Feature/ExampleTest.php
+++ projet-produits/tests/Feature/ExampleTest.php
@@ -2,16 +2,20 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase; // <--- AJOUTE CETTE LIGNE
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase; // <--- AJOUTE CETTE LIGNE AUSSI
+
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
+        // Maintenant, RefreshDatabase va créer 'produits_table'
+        // juste avant de faire ce GET
         $response = $this->get('/');
 
         $response->assertStatus(200);
