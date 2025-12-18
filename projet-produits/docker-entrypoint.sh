89fe6803596a51db0f214f16c5ca5d5f222c1f7a
Noan_Bregeon <furienocturne2.0@gmail.com>
2025-12-18 12:29:25 +0100

    Update docker-entrypoint.sh

--- projet-produits/docker-entrypoint.sh
+++ projet-produits/docker-entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-php artisan migrate --force
+# Nettoyer les vieux fichiers de cache qui pourraient être restés dans l'image
+php artisan config:clear
+php artisan cache:clear
+
+echo "Waiting for database..."
+sleep 5
+
+php artisan migrate:fresh --force # Utilise :fresh pour être CERTAIN d'écraser l'existant
 php artisan db:seed --force
+
 exec "$@"
