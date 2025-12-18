9bfce761faa059d9eadf9a1c61e09dc2893a9694
Jules-pecquereau <jules.pecquereau4@gmail.com>
2025-12-18 14:34:23 +0100

    Update docker-entrypoint.sh

--- projet-produits/docker-entrypoint.sh
+++ projet-produits/docker-entrypoint.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
-# On attend que le service "mysql" réponde sur le port 3306
-while ! nc -z mysql 3306; do
-	echo "Attente de MySQL..."
-	sleep 1
-done
-
 # Nettoyer les vieux fichiers de cache qui pourraient être restés dans l'image
 php artisan config:clear
 php artisan cache:clear
 
-# Migrer et remplir la base
+echo "Waiting for database..."
+sleep 5
+
 php artisan migrate:fresh --force # Utilise :fresh pour être CERTAIN d'écraser l'existant
 php artisan db:seed --force
 
