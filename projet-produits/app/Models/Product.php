8bb3ccfdf8a44fefd7e2233e2d704594e7cb0ebd
Noan_Bregeon <furienocturne2.0@gmail.com>
2025-12-18 12:29:21 +0100

    Update Product.php

--- projet-produits/app/Models/Product.php
+++ projet-produits/app/Models/Product.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $table = 'produits_table';
-    public $timestamps = false;
+    protected $primaryKey = 'id'; // On précise que c'est 'id'
+    public $incrementing = true;  // On force l'auto-incrément au niveau Laravel
+    protected $keyType = 'int';   // On précise que c'est un entier
 
-    // SANS CETTE LIGNE, LE "CREATE" NE MARCHERA PAS
+    public $timestamps = false;
     protected $fillable = ['nom', 'prix'];
 }
