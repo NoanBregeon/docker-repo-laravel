22c2ea3f8ae4fe9f8f175885865ac926cf90b0e4
Jules-pecquereau <jules.pecquereau4@gmail.com>
2025-12-18 11:16:30 +0100

    ajout du init-mysql et modif des docker file

---
+++ projet-produits/init-mysql.sql
@@ -0,0 +1,14 @@
+CREATE database IF NOT EXISTS produits;
+USE produits;
+CREATE TABLE IF NOT EXISTS produits_table (
+    id int NOT NULL PRIMARY KEY,
+    nom varchar(255) NOT NULL,
+    prix decimal(10,2) NOT NULL
+);
+
+INSERT INTO produits_table VALUES
+    (1, 'deo', 2.99),
+    (2, 'pain ', 0.99 ),
+    (3, 'oeuf', 3.49 ),
+    (4, 'fromage', 4.99 )
+;
